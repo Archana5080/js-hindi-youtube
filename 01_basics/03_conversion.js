552ad38b5755ebbdc27537cd1c0091108b65b31a
Archana5080 <151072129+Archana5080@users.noreply.github.com>
2024-05-22 17:28:53 +0000

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -2,9 +2,9 @@ let score = false
 console.log(typeof score);
 console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
 
 
 
@@ -19,6 +19,34 @@ console.log(valueInNumber);
 //"" => false
 // "shubham" => true
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let isLoggedIn = 1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//
+//console.log(booleanIsLoggedIn);
+
+
+//********************operation*************** */
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "shubham"
+let str2 = "dubey"
+
+let str3 = str1+ " " +str2
+console.log(str3);
+
+console.log("1"+2);
+console.log("1"+"2");
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log((3+4)*5%3);
+
+console.log(true);
+
+let num1, num2 , num3
+
+num1 =num2= num3 = 2+2      //its not good
