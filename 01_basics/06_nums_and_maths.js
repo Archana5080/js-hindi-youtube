ba619abc7a0634ccb3392f6804fd5e51ee036fc8
Archana5080 <151072129+Archana5080@users.noreply.github.com>
2024-05-23 09:28:22 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number (100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+  //console.log(balance.toFixed(2));
+
+const otherNumber = 236;
+
+//console.log(otherNumber.toPrecision(3));
+
+
+//********************Maths************** */
+
+//console.log(Math);
+//console.log(Math.abs(-4));              // positive valve 4
+ //console.log(Math.round(4.6));         // in same line
+// console.log(Math.ceil(4.2));         // roundoff to higest       //output 5
+// console.log(Math.floor(4.9));         // round off to lowest //output = 4
+// console.log(Math.pow(2,3));            // for power
+// console.log(Math.random(9));
+// console.log(Math.min(2,3,7,9,8,1));
+
+// random
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+ console.log(Math.floor(Math.random() * (max - min +1)) + min)
