13fbe48517f03f5249ad14f2f0b4a202b7930940
Archana5080 <151072129+Archana5080@users.noreply.github.com>
2024-05-23 05:08:39 +0000

    datatype.summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+// promitive
+
+// t types: string , Number , boolean , null , undefined , symbol,bigInt
+const score = 100;
+const scoreValue = 100.8;
+
+const isLoggedIn = false;
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+
+ const bigNumber = 23344657650988766n
+
+// reference (Non primitive)
+
+// Array,Objects , Function
+
+const neros = ["shaktman","neeraj","spiderman"];
+
+let myOnj = {
+    name: "shubham",
+    age: 20,
+}
+
+const myFunction = function(){
+console.log("he worldllo");
+}
+
+console.log(typeof bogNumber)
+
