2f310fd13e56564e63b010f1cc15a1ecba4e01ac
Archana5080 <151072129+Archana5080@users.noreply.github.com>
2024-05-27 11:48:37 +0000

    array

---
+++ 02_bas/02_array.js
@@ -0,0 +1,11 @@
+const marvel_heroes = ["thor", "ironman","spiderman"]
+const dc_heros =["superman","flash","batman"]
+
+//marvel_heroes.push(dc_heros)
+
+//console.log(marvel_heroes);
+//console.log(marvel_heroes[3][1]);
+
+
+marvel_heroes.concat(dc_heros)
+console.log(marvel_heroes);
