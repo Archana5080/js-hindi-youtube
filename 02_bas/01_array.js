e624e10cb59bf0fcb08646ea1b7f1bcb7084c0fa
Archana5080 <151072129+Archana5080@users.noreply.github.com>
2024-05-27 11:34:54 +0000

    baisc array

---
+++ 02_bas/01_array.js
@@ -0,0 +1,20 @@
+// Array
+
+//    Array methods
+// Push = add to end
+const myHeroes = ["shaktiman","marvel","naagraaj"]
+ const hero= myHeroes.push("spiderman")
+console.log(hero)
+
+// pop = delete from end and return it
+// unshift =  add to start
+// shift = delete from end
+// indexOf = returns index of something
+// includes = search for a value
+// concate: merge two array
+// reverse = reverse an array
+// slice = copies a portion of of an array 
+// splice =  remove / replace/ add element in place
+// sort = sorts an array
+
+
